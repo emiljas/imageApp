de010118161a872accde249e279f7d84b3c393ae
Emil Jasinski <emiljasinski@gmail.com>
2014-09-05 00:14:32 +0200

    working crop

--- ImageApp/ImageEditorPage/ImageEditor.xaml.cs
+++ ImageApp/ImageEditorPage/ImageEditor.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ImageApp.Common;
+﻿using CSWindowsStoreAppCropBitmap;
+using ImageApp.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,8 +8,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,7 +25,11 @@ namespace ImageApp.ImageEditorPage
 {
     public sealed partial class ImageEditor : Page
     {
-        private WriteableBitmap _editBitmap;
+        private WriteableBitmap editBitmap;
+        private SelectedRegion selectedRegion;
+
+        private uint sourceImagePixelWidth;
+        private uint sourceImagePixelHeight;
 
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
@@ -37,46 +44,51 @@ namespace ImageApp.ImageEditorPage
             get { return this.navigationHelper; }
         }
 
+        private Lazy<double> cornerSize = new Lazy<double>(delegate()
+            {
+                return (double)Application.Current.Resources["Size"];
+            });
+
+
+        private Dictionary<uint, Point?> pointerPositionHistory = new Dictionary<uint, Point?>();
+
         public ImageEditor()
         {
             this.InitializeComponent();
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+            selectRegion.ManipulationMode = ManipulationModes.Scale
+                                            | ManipulationModes.TranslateX
+                                            | ManipulationModes.TranslateY;
+            selectedRegion = new SelectedRegion { MinSelectRegionSize = 2 * cornerSize.Value };
+            this.DataContext = selectedRegion;
         }
 
         private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
 
             var imageToEditPath = e.NavigationParameter as string;
-            _editBitmap = await LoadWritableBitmap(imageToEditPath);
-            EditImage.Source = _editBitmap;
-            FitImageToCanvas();
-        }
+            editBitmap = await LoadWritableBitmap(imageToEditPath);
+            sourceImage.Source = editBitmap;
 
-        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
+            this.sourceImagePixelWidth = (uint)editBitmap.PixelWidth;
+            this.sourceImagePixelHeight = (uint)editBitmap.PixelHeight;
 
-        private void FitImageToCanvas()
-        {
-            if (_editBitmap == null || EditImage.ActualWidth == 0)
-                return;
+            double sourceImageScale = 1;
 
-            bool isImageBiggerThanPage = _editBitmap.PixelWidth > this.ActualWidth || _editBitmap.PixelHeight > this.ActualHeight;
-            if(isImageBiggerThanPage)
+            if (this.sourceImagePixelHeight < this.sourceImageGrid.ActualHeight &&
+                this.sourceImagePixelWidth < this.sourceImageGrid.ActualWidth)
             {
+                this.sourceImage.Stretch = Windows.UI.Xaml.Media.Stretch.None;
             }
             else
             {
-                Canvas.SetLeft(EditImage, this.ActualWidth / 2 - EditImage.ActualWidth / 2);
+                sourceImageScale = Math.Min(this.sourceImageGrid.ActualWidth / this.sourceImagePixelWidth,
+                     this.sourceImageGrid.ActualHeight / this.sourceImagePixelHeight);
+                this.sourceImage.Stretch = Windows.UI.Xaml.Media.Stretch.Uniform;
             }
-        }
-
-        private void EditImage_Loaded(object sender, RoutedEventArgs e)
-        {
-            FitImageToCanvas();
         }
 
         private async Task<WriteableBitmap> LoadWritableBitmap(string path)
@@ -110,6 +122,117 @@ namespace ImageApp.ImageEditorPage
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedTo(e);
+
+            selectedRegion.PropertyChanged += selectedRegion_PropertyChanged;
+
+            AddCornerEvents(topLeftCorner);
+            AddCornerEvents(topRightCorner);
+            AddCornerEvents(bottomLeftCorner);
+            AddCornerEvents(bottomRightCorner);
+
+            selectRegion.ManipulationDelta += selectRegion_ManipulationDelta;
+            selectRegion.ManipulationCompleted += selectRegion_ManipulationCompleted;
+
+            this.sourceImage.SizeChanged += sourceImage_SizeChanged;
+        }
+
+        private void sourceImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.IsEmpty || double.IsNaN(e.NewSize.Height) || e.NewSize.Height <= 0)
+            {
+                this.imageCanvas.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                this.selectedRegion.OuterRect = Rect.Empty;
+                this.selectedRegion.ResetCorner(0, 0, 0, 0);
+            }
+            else
+            {
+                this.imageCanvas.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                this.imageCanvas.Height = e.NewSize.Height;
+                this.imageCanvas.Width = e.NewSize.Width;
+                this.selectedRegion.OuterRect = new Rect(0, 0, e.NewSize.Width, e.NewSize.Height);
+
+                if (e.PreviousSize.IsEmpty || double.IsNaN(e.PreviousSize.Height) || e.PreviousSize.Height <= 0)
+                {
+                    this.selectedRegion.ResetCorner(0, 0, e.NewSize.Width, e.NewSize.Height);
+                }
+                else
+                {
+                    double scale = e.NewSize.Height / e.PreviousSize.Height;
+                    this.selectedRegion.ResizeSelectedRect(scale);
+                }
+            }
+        }
+
+        private void selectRegion_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            this.selectedRegion.UpdateSelectedRect(e.Delta.Scale, e.Delta.Translation.X, e.Delta.Translation.Y);
+            e.Handled = true;
+        }
+
+        private void selectRegion_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+        }
+
+        private void AddCornerEvents(Control corner)
+        {
+            corner.PointerPressed += corner_PointerPressed;
+            corner.PointerMoved += corner_PointerMoved;
+            corner.PointerReleased += corner_PointerReleased;
+        }
+
+        private void RemoveCornerEvents(Control corner)
+        {
+            corner.PointerPressed -= corner_PointerPressed;
+            corner.PointerMoved -= corner_PointerMoved;
+            corner.PointerReleased -= corner_PointerReleased;
+        }
+
+        private void corner_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            (sender as UIElement).CapturePointer(e.Pointer);
+            PointerPoint point = e.GetCurrentPoint(this);
+            pointerPositionHistory[point.PointerId] = point.Position;
+            e.Handled = true;
+        }
+
+        private void corner_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            PointerPoint point = e.GetCurrentPoint(this);
+            uint pointerId = point.PointerId;
+
+            if (pointerPositionHistory.ContainsKey(pointerId) && pointerPositionHistory[pointerId].HasValue)
+            {
+                Point currentPosition = point.Position;
+                Point previousPosition = pointerPositionHistory[pointerId].Value;
+
+                double xUpdate = currentPosition.X - previousPosition.X;
+                double yUpdate = currentPosition.Y - previousPosition.Y;
+
+                this.selectedRegion.UpdateCorner((sender as ContentControl).Tag as string, xUpdate, yUpdate);
+
+                pointerPositionHistory[pointerId] = currentPosition;
+            }
+
+            e.Handled = true;
+        }
+
+        private void corner_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            uint ptrId = e.GetCurrentPoint(this).PointerId;
+            if (this.pointerPositionHistory.ContainsKey(ptrId))
+            {
+                this.pointerPositionHistory.Remove(ptrId);
+            }
+
+            (sender as UIElement).ReleasePointerCapture(e.Pointer);
+            e.Handled = true;
+        }
+
+        void selectedRegion_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            double widthScale = imageCanvas.Width / this.sourceImagePixelWidth;
+            double heightScale = imageCanvas.Height / this.sourceImagePixelHeight;
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -121,60 +244,35 @@ namespace ImageApp.ImageEditorPage
 
         private void Negative_Click(object sender, RoutedEventArgs e)
         {
-            _editBitmap = _editBitmap.Invert();
-            EditImage.Source = _editBitmap;
+            editBitmap = editBitmap.Invert();
+            sourceImage.Source = editBitmap;
         }
         private void Brightness_Click(object sender, RoutedEventArgs e)
         {
         }
         private void Crop_Click(object sender, RoutedEventArgs e)
         {
+            double sourceImageWidthScale = imageCanvas.Width / this.sourceImagePixelWidth;
+            double sourceImageHeightScale = imageCanvas.Height / this.sourceImagePixelHeight;
+            var rect = new Rect();
+            rect.X = this.selectedRegion.SelectedRect.X / sourceImageWidthScale;
+            rect.Y = this.selectedRegion.SelectedRect.Y / sourceImageHeightScale;
+            rect.Width = this.selectedRegion.SelectedRect.Width / sourceImageWidthScale;
+            rect.Height = this.selectedRegion.SelectedRect.Height / sourceImageHeightScale;
+            sourceImage.Source = editBitmap.Crop(rect);
+
+            selectRegion.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            nonselectRegion.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            topLeftCorner.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            topRightCorner.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            bottomLeftCorner.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            bottomRightCorner.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
         }
         private void Save_Click(object sender, RoutedEventArgs e)
         {
         }
         private void Close_Click(object sender, RoutedEventArgs e)
         {
-        }
-
-        private void EditImage_ImageOpened(object sender, RoutedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void EditImage_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void EditImage_Unloaded(object sender, RoutedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void pageRoot_LayoutUpdated(object sender, object e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void pageRoot_Loaded(object sender, RoutedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void pageRoot_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void Canvas_Loaded(object sender, RoutedEventArgs e)
-        {
-            FitImageToCanvas();
-        }
-
-        private void Canvas_LayoutUpdated(object sender, object e)
-        {
-            FitImageToCanvas();
         }
     }
 }
