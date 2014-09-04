29167c17a59f12b4f8e42c80a8a53f790d8619d2
Emil Jasinski <emiljasinski@gmail.com>
2014-09-04 15:41:42 +0200

    before centring canvas

--- ImageApp/ImageEditorPage/ImageEditor.xaml.cs
+++ ImageApp/ImageEditorPage/ImageEditor.xaml.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,6 +22,7 @@ namespace ImageApp.ImageEditorPage
 {
     public sealed partial class ImageEditor : Page
     {
+        private WriteableBitmap _editBitmap;
 
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
@@ -44,17 +47,58 @@ namespace ImageApp.ImageEditorPage
 
         private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
+
             var imageToEditPath = e.NavigationParameter as string;
-            var file = await StorageFile.GetFileFromPathAsync(imageToEditPath);
-            var bitmap = new BitmapImage();
-            bitmap.SetSource(await file.OpenReadAsync());
-            EditImage.Source = bitmap;
+            _editBitmap = await LoadWritableBitmap(imageToEditPath);
+            EditImage.Source = _editBitmap;
+            FitImageToCanvas();
+        }
 
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
 
-            var w = new WriteableBitmap(bitmap.PixelWidth, bitmap.PixelHeight);
-            w.SetSource(await file.OpenReadAsync());
-            var ww = w.Invert();
-            EditImage.Source = ww;
+        private void FitImageToCanvas()
+        {
+            if (_editBitmap == null || EditImage.ActualWidth == 0)
+                return;
+
+            bool isImageBiggerThanPage = _editBitmap.PixelWidth > this.ActualWidth || _editBitmap.PixelHeight > this.ActualHeight;
+            if(isImageBiggerThanPage)
+            {
+            }
+            else
+            {
+                Canvas.SetLeft(EditImage, this.ActualWidth / 2 - EditImage.ActualWidth / 2);
+            }
+        }
+
+        private void EditImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private async Task<WriteableBitmap> LoadWritableBitmap(string path)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(path);
+            var data = await FileIO.ReadBufferAsync(file);
+
+            var ms = new InMemoryRandomAccessStream();
+            var dw = new Windows.Storage.Streams.DataWriter(ms);
+            dw.WriteBuffer(data);
+            await dw.StoreAsync();
+            ms.Seek(0);
+
+            var bm = new BitmapImage();
+            await bm.SetSourceAsync(ms);
+
+            var wb = new WriteableBitmap(bm.PixelWidth, bm.PixelHeight);
+            ms.Seek(0);
+
+            await wb.SetSourceAsync(ms);
+
+            return wb;
         }
 
         private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
@@ -77,6 +121,8 @@ namespace ImageApp.ImageEditorPage
 
         private void Negative_Click(object sender, RoutedEventArgs e)
         {
+            _editBitmap = _editBitmap.Invert();
+            EditImage.Source = _editBitmap;
         }
         private void Brightness_Click(object sender, RoutedEventArgs e)
         {
@@ -89,6 +135,46 @@ namespace ImageApp.ImageEditorPage
         }
         private void Close_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void EditImage_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void EditImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void EditImage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void pageRoot_LayoutUpdated(object sender, object e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void pageRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void pageRoot_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void Canvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            FitImageToCanvas();
+        }
+
+        private void Canvas_LayoutUpdated(object sender, object e)
+        {
+            FitImageToCanvas();
         }
     }
 }
